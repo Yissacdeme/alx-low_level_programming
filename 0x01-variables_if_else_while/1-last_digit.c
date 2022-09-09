cc54e6f33de7ec369da92786ef5f5fd9937202ad
Yissacdeme <yisakdemeke123@gmail.com>
2022-09-09 16:47:52 -0700

    qwertyuiop

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@
 
 /**
  *
- *  * main - Prints the last digit of a randomly generated number
+ * main - Prints the last digit of a randomly generated number
  *
  *   *        and whether it is greater than 5, less than 6, or 0.
  *
