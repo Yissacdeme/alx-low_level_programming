108266e60995c14cf387bcf86e4e712a2a615b71
Yissacdeme <yisakdemeke123@gmail.com>
2022-09-09 18:10:21 -0700

    4th task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -22,9 +22,8 @@ int main(void)
 
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
-		
-		if (letter != 'e' && letter != 'q')					  putchar(letter);
-
+		if (letter != 'e' && letter != 'q')					  
+			putchar(letter);
 		}
 		putchar('\n');
 		return (0);
