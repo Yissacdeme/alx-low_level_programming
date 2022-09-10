4c39a147451ebf0af7efcac122635d752561ef6e
Yissacdeme <yisakdemeke123@gmail.com>
2022-09-09 17:28:48 -0700

    printalpa

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -22,7 +22,8 @@ int main(void)
 {
 		char letter;
 
-	        for (letter = 'a'; letter <= 'z'; letter++) putchar(letter);
+	        for (letter = 'a'; letter <= 'z'; letter++)
+		       	putchar(letter);
 
 		putchar('\n');
 
