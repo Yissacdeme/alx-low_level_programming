cba790c8ac7d12bc413ba0ab8f72df655f63c131
Yissacdeme <yisakdemeke123@gmail.com>
2022-09-09 19:57:20 -0700

    101th

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,8 +11,8 @@
 /**
  * main - Prints all possible combinations of three different digits,
  *
- *   *        in ascending order, separated by a comma followed by a space.
- *     * Return: Always 0.
+ * in ascending order, separated by a comma followed by a space.
+ *   Return: Always 0.
  */
 
 int main(void)
