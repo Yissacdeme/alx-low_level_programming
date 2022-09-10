6e69817321d62d5fbc707b252dd18480cca577be
Yissacdeme <yisakdemeke123@gmail.com>
2022-09-09 18:54:33 -0700

    8th

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -17,14 +17,14 @@
 int main(void)
 
 {
-	        int num;
+	        int num;	
 		char letter;
-
+		
 		for (num = 0; num < 10; num++)
-			putchar((num % 10) + '0');
+			putchar((num % 10) + '0');	
 
 		for (letter = 'a'; letter <= 'f'; letter++)
-			putchar(letter);
-		putchar('\n');
+			putchar(letter);	
+		putchar('\n');	
 		return (0);
-}
+}	
