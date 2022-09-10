f2b392ae6cf56ad41c8d8ed6c479f34bf0ece3c4
Yissacdeme <yisakdemeke123@gmail.com>
2022-09-09 19:14:36 -0700

    9th

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -24,10 +24,12 @@ int main(void)
 		for (num = 0; num <= 9; num++)
 
 		{
-			putchar((num % 10) + '0');					    if (num == 9)
+			putchar((num % 10) + '0');					    	
+			if (num == 9)
 				continue;						    
-			putchar(',');							    putchar(' ');
-		}
+			putchar(',');							   
+		       	putchar(' ');
+	        }
 
 		putchar('\n');
 
