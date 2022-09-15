7499dfd52e680df94bb97857ac8089030b4fe0ea
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2022-09-15 16:28:48 -0700

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,16 @@
-wertyuiopdfghjkvcbnmcrtvybunjircvgybuhnji
+#include "holberton.h"
+
+/**
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
