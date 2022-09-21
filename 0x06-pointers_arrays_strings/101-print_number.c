6bcda28b6b3d4e08d2f82282b507cdfc1f45f05c
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2022-09-21 15:13:50 -0700

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
-* print_number - prints an integer.
-* @n: The integer to be printed.
-*/
+ * print_number -print number putchar
+ * @n:integer
+ * Return: void
+ */
 void print_number(int n)
 {
-  unsigned int num = n;
-  
-  if (n < 0)
-  {
-    _putchar(' - ');
-    num = -num;
-  }
-  
-  if((num/10) > 0)
-  print_number(num/10);
-  
-  _putchar((num % 10) + '0');
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
