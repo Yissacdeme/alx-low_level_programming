a7f3209f20b1cd30ec1bef71c413a74d72bf03c5
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2022-10-11 08:51:38 -0700

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,14 @@
-#ifndef DOG_H
-#define DOG_H
-
-/*
- * File: dog.h
- * Auth: Brennan D Baraban
- * Desc: Header file that defines a new type struct dog.
- */
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
- * struct dog - A new type describing a dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- */
+  * struct dog - Dog attributes
+  * @name: The name of the dog.
+  * @age: The age of the dog.
+  * @owner: The owner of the dog.
+  *
+  * Description: The attributes of a dog.
+  */
 struct dog
 {
 	char *name;
@@ -21,7 +17,7 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for struct dog
+ * dog_t - Typedef for dog structure
  */
 typedef struct dog dog_t;
 
@@ -29,5 +25,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
