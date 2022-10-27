c7ce2bdb17e096590fd5ee87c48b37de2b942e8a
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2022-10-27 04:15:32 -0700

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,32 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
+#include <math.h>
 
 /**
-  * binary_to_uint - Converts a binary number to an unsigned int
-  * @b: The binary string to converts
-  *
-  * Return: The positive number converted from a binary
-  */
+ * binary_to_uint - converts binary to uint
+ * @b: pointer to the character array in binary to be converted
+ *
+ * Return: converted number if successful, 0 otherwise
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+	int i, base = 1, len = 0;
+	unsigned int res = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	len = _strlen(b);
-	while (len--)
+	len = strlen(b);
+
+	for (i = len - 1; i >= 0; i--)
 	{
-		if (b[len] != 48 && b[len] != 49)
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
-		if (b[len] == 49)
-			sum += 1 << count;
-
-		count++;
+		if (b[i] == '1')
+			res += base;
+		base *= 2;
 	}
-
-	return (sum);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
+	return (res);
 }
